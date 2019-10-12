bdf0eadcdbe757131f754b8337c71968a2d04a7e
Manchangdx <1195581533@qq.com>
2019-10-12 14:08:55 +0800

    解决多个 Bug

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[实验楼](http://www.shiyanlou.com)项目课程 - Web即时通信：基于Server-Sent Event的简单聊天室
+[实验楼](http://www.shiyanlou.com)项目课程 - Web 即时通信：基于 Server-Sent Event 实现聊天室
--- app.py
+++ app.py
@@ -1,80 +1,53 @@
-#!/usr/bin/env python
 import datetime
 import flask
 import redis
 
+app = flask.Flask('shiyanlou-sse-chat')
+app.secret_key = 'shiyanlou'
+app.config['DEBUG'] = True
+r = redis.StrictRedis()
 
-app = flask.Flask(__name__)
-app.secret_key = 'asdf'
-red = redis.StrictRedis(host='localhost', port=6379, db=6)
+# 主页路由函数
+@app.route('/')
+def home():
+    # 如果用户未登录，重定向到登录页面
+    if 'user' not in flask.session:
+        return flask.redirect('/login')
+    user = flask.session['user']
+    return flask.render_template('home.html', user=user)
 
-
+# 消息生成器
 def event_stream():
-    pubsub = red.pubsub()
+    # 创建发布订阅系统
+    pubsub = r.pubsub()
+    # 使用发布订阅系统的 subscribe 方法订阅某个频道
     pubsub.subscribe('chat')
-    # TODO: handle client disconnection.
     for message in pubsub.listen():
-        print message
-        yield 'data: %s\n\n' % message['data']
+        data = message['data']
+        if type(data) == bytes:
+            yield 'data: {}\n\n'.format(data.decode())
 
-
+# 登录函数，首次访问需要登录
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'POST':
+        # 将用户名存到 session 字典里，然后重定向到首页
         flask.session['user'] = flask.request.form['user']
         return flask.redirect('/')
-    return '<form action="" method="post">user: <input name="user">'
+    return ('<form action="" method="post">User Name: <input name="user"> '
+            '<input type="submit" value="login" /></form>')
 
-
+# 接收 JavaScript 使用 POST 方法发送的数据
 @app.route('/post', methods=['POST'])
 def post():
     message = flask.request.form['message']
     user = flask.session.get('user', 'anonymous')
     now = datetime.datetime.now().replace(microsecond=0).time()
-    red.publish('chat', u'[%s] %s: %s' % (now.isoformat(), user, message))
+    r.publish('chat', '[{}] {}: {}\n'.format(now.isoformat(), user, message))
     return flask.Response(status=204)
 
-
+# 事件流接口
 @app.route('/stream')
 def stream():
-    return flask.Response(event_stream(),
-                          mimetype="text/event-stream")
-
-
-@app.route('/')
-def home():
-    if 'user' not in flask.session:
-        return flask.redirect('/login')
-    return u"""
-        <!doctype html>
-        <title>chat</title>
-        <script src="http://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
-        <style>body { max-width: 500px; margin: auto; padding: 1em; background: black; color: #fff; font: 16px/1.6 menlo, monospace; }</style>
-        <p><b>hi, %s!</b></p>
-        <p>Message: <input id="in" /></p>
-        <pre id="out"></pre>
-        <script>
-            function sse() {
-                var source = new EventSource('/stream');
-                var out = document.getElementById('out');
-                source.onmessage = function(e) {
-                    // XSS in chat is fun
-                    out.innerHTML =  e.data + '\\n' + out.innerHTML;
-                };
-            }
-            $('#in').keyup(function(e){
-                if (e.keyCode == 13) {
-                    $.post('/post', {'message': $(this).val()});
-                    $(this).val('');
-                }
-            });
-            sse();
-        </script>
-
-    """ % flask.session['user']
-
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8989, threaded=True)
-
+    # 该路由函数返回对象的数据类型须是 text/event-stream 类型
+    return flask.Response(event_stream(), mimetype='text/event-stream')
